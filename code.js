72bd7a5b3d4ef029e81826a6b094a368d121209b
Aheer Alvi <aheeralvi@Aheers-MacBook-Pro.local>
2017-06-13 14:19:23 -0400

    Added functionality to the left/right buttons, they can be used to change the month visible on the top

---
+++ code.js
@@ -0,0 +1,20 @@
+// create an array to store the names of the months
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var currentMonth = 0;
+
+//function that acts onclick when right calendar button is clicked
+function nextmonth() {
+    currentMonth++;
+    if (currentMonth == 12) {
+        currentMonth = 0;
+    }
+    document.getElementById('month').innerText = months[currentMonth];
+}
+
+function prevmonth() {
+    currentMonth--;
+    if (currentMonth == -1) {
+        currentMonth = 11;
+    }
+    document.getElementById('month').innerText = months[currentMonth];
+}
